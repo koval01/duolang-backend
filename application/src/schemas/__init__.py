a2a90f1d12ec1d5dc0f591e8499a0a56a4337db3
Koval Yaroslav <yaroslav@koval.page>
2024-08-23 17:59:14 +0200

    Update schemas

--- application/src/schemas/__init__.py
+++ application/src/schemas/__init__.py
@@ -1,4 +1,4 @@
-from application.src.schemas.misc import ResultBody
-from application.src.schemas.profile import ProfileItem, ProfileItemInit, ProfileItemCreate, ProfileItemDisplay, ProfileItemUpdate
-from application.src.schemas.prompt import Prompt
-from application.src.schemas.lesson import Lesson, Task, TaskTypeEnum
+from .misc import ResultBody
+from .profile import ProfileItem, ProfileItemInit, ProfileItemCreate, ProfileItemDisplay, ProfileItemUpdate
+from .prompt import Prompt
+from .lesson import Lesson, Task, TaskTypeEnum
