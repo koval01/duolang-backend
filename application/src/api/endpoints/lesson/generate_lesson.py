83c1d53752ca3961941261d67ca9d819ffe18e40
Koval Yaroslav <yaroslav@koval.page>
2024-08-24 10:22:03 +0200

    Commit generate_lesson.py

--- application/src/api/endpoints/lesson/generate_lesson.py
+++ application/src/api/endpoints/lesson/generate_lesson.py
@@ -9,8 +9,8 @@ from sqlalchemy import select
 from pydantic import ValidationError, BaseModel
 
 from application.src.gemini import Gemini
-from application.src.models import Lesson, Profile
-from application.src.schemas import Lesson as LessonSchema
+from application.src.models import Profile, Lesson, Task
+from application.src.schemas import Lesson as LessonSchema, LessonResponse
 
 router = APIRouter()
 
@@ -20,7 +20,7 @@ class InitRequestBody(BaseModel):
 
 
 @router.post("/generate", response_model=LessonSchema)
-async def generate_lesson(request: Request, _: InitRequestBody) -> LessonSchema:
+async def generate_lesson(request: Request, _: InitRequestBody) -> LessonResponse:
     web_app_init_data: WebAppInitData = request.state.web_app_init_data
 
     # Validate request body
@@ -36,15 +36,30 @@ async def generate_lesson(request: Request, _: InitRequestBody) -> LessonSchema:
     if not profile:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Profile does not exist")
 
-    # Generate lesson using Gemini (mocked for example)
-    generated_lesson: Lesson = Gemini().execute(body.course)
+    # Check if there are any incomplete lessons for the same course
+    existing_lesson_query = select(Lesson).where(
+        Lesson.profile_id == profile.id,
+        Lesson.course == body.course,
+        Lesson.completed == False
+    )
+    existing_lesson_result = await db.session.execute(existing_lesson_query)
+    existing_lesson = existing_lesson_result.scalars().first()
+    # if existing_lesson:
+    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="There is an incomplete lesson for this course")
 
-    # Store lesson in the database
-    new_lesson = LessonSchema(profile_id=profile.id, tasks=generated_lesson.tasks)
+    # Generate lesson using the neural network
+    generated_lesson: LessonSchema = Gemini().execute(body.course)
+
+    # Save the lesson to the database
+    new_lesson = Lesson(
+        profile_id=profile.id,
+        course=body.course,
+        completed=False,
+        tasks=[Task(**task.model_dump()) for task in generated_lesson.tasks]
+    )
+
     db.session.add(new_lesson)
     await db.session.commit()
+    await db.session.refresh(new_lesson)
 
-    for task in new_lesson.tasks:
-        del task.answer
-
-    return new_lesson
+    return LessonResponse(tasks=generated_lesson.tasks)
