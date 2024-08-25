71e2af0d2237c576c8ac8227de4d447c826c1678
Koval Yaroslav <yaroslav@koval.page>
2024-08-25 18:50:03 +0200

    Update solve schema

--- application/src/schemas/__init__.py
+++ application/src/schemas/__init__.py
@@ -2,3 +2,4 @@ from .misc import ResultBody
 from .profile import ProfileItem, ProfileItemInit, ProfileItemCreate, ProfileItemDisplay, ProfileItemUpdate
 from .prompt import Prompt
 from .lesson import Lesson, Task, TaskTypeEnum
+from .solve import UserAnswers
--- application/src/schemas/solve.py
+++ application/src/schemas/solve.py
@@ -1,12 +1,35 @@
 from pydantic import BaseModel
 from typing import List, Dict, Union
+from typing_extensions import Literal
 
 
-class TaskResponse(BaseModel):
-    task_id: int
-    user_answer: Union[int, str, List[str], Dict[str, str]]
+class BaseTaskAnswer(BaseModel):
+    task_type: Literal["translation", "fill-in", "multiple-choice", "matching", "rearrange"]
 
 
-class LessonResponse(BaseModel):
-    lesson_id: int
-    responses: List[TaskResponse]
+class TranslationAnswer(BaseTaskAnswer):
+    task_type: Literal["translation"] = "translation"
+
+
+class FillInAnswer(BaseTaskAnswer):
+    task_type: Literal["fill-in"] = "fill-in"
+    answer: int
+
+
+class MultipleChoiceAnswer(BaseTaskAnswer):
+    task_type: Literal["multiple-choice"] = "multiple-choice"
+    answer: int
+
+
+class MatchingAnswer(BaseTaskAnswer):
+    task_type: Literal["matching"] = "matching"
+    pairs: List[Dict[str, str]]
+
+
+class RearrangeAnswer(BaseTaskAnswer):
+    task_type: Literal["rearrange"] = "rearrange"
+    sentence: str
+
+
+class UserAnswers(BaseModel):
+    answers: List[Union[TranslationAnswer, FillInAnswer, MultipleChoiceAnswer, MatchingAnswer, RearrangeAnswer]]
